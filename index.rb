55ba2acd55a85113486a76f87f06e0c92491eacf
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-09-07 18:27:58 +0430

    solve linter errors

--- index.rb
+++ index.rb
@@ -15,17 +15,16 @@ def decode_char(morse)
 end
 
 def decode_word(morse)
-  morse_characters = morse.split(' ')
-  decoded_word = morse_characters.map { |s| decode_char(s) }.join('')
-  decoded_word
+  morse_characters = morse.split
+  morse_characters.map { |s| decode_char(s) }.join
 end
+
 def decode_message(morse)
   morse_words = morse.split('   ')
-  decoded_message = morse_words.map { |s| decode_word(s) }.join(' ')
-  decoded_message
+  morse_words.map { |s| decode_word(s) }.join(' ')
 end
 
 # Example usages
-puts decode_char(".-") 
-puts decode_word("-- -.--") 
-puts decode_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts decode_char('.-')
+puts decode_word('-- -.--')
+puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
